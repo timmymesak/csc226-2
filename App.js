03c5b8a8963899ce5ddf3159a3c689072e8047a0
timmymesak <124318001+timmymesak@users.noreply.github.com>
2023-12-19 16:48:49 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import MyApiComponent from './MyApiComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Your React App</h1>
+      <MyApiComponent />
+    </div>
+  );
+};
+
+export default App;
